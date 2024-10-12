82bd7d7671a08a5639573c42821f90fa2b5c0d5f
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-10-12 16:43:08 +0200

    tests

--- main.js
+++ main.js
@@ -1,75 +1,72 @@
 import { GLTFLoader, OrbitControls } from "three/examples/jsm/Addons.js";
 import "./style.css";
 import * as THREE from "three";
-import { Pane } from "tweakpane";
-import { BloomPass } from "three/examples/jsm/postprocessing/BloomPass.js";
 
 //initialize the scene
 const scene = new THREE.Scene();
+
+let randomMesh;
+let rotatingMeshes = [];
 
 //add material
 const cubeMaterial = new THREE.MeshPhysicalMaterial();
 cubeMaterial.color = new THREE.Color(0xef5930);
 cubeMaterial.clearcoat = 1;
+
 const faceMaterial = new THREE.MeshPhysicalMaterial();
-faceMaterial.color = new THREE.Color(0x456456);
+faceMaterial.color = new THREE.Color(0x000);
 faceMaterial.clearcoat = 1;
 
-//add geometry
+//add head geometry
 const sphereGeometry = new THREE.SphereGeometry(0.6, 40, 40);
-const sphereGeometry2 = new THREE.SphereGeometry(0.3, 40, 40);
-const torusKnotGeometry = new THREE.TorusKnotGeometry(0.4, 0.15, 100, 16);
-const coneGeometry = new THREE.ConeGeometry(0.6, 1.2, 25);
-
 const sphereMesh = new THREE.Mesh(sphereGeometry, cubeMaterial);
-let sphereMesh2 = new THREE.Mesh(sphereGeometry2, faceMaterial);
-const torusKnotMesh = new THREE.Mesh(torusKnotGeometry, cubeMaterial);
-const coneMesh = new THREE.Mesh(coneGeometry, cubeMaterial);
-sphereMesh2 = new THREE.Mesh(sphereGeometry2, cubeMaterial);
+
+const emojiGeometry = new THREE.SphereGeometry(0.3, 40, 40);
+let sphereMesh2 = new THREE.Mesh(emojiGeometry, faceMaterial);
+
 sphereMesh2.position.z = 0.8;
+
+//add emoji to face
 sphereMesh.add(sphereMesh2);
-scene.add(sphereMesh, torusKnotMesh, coneMesh);
+scene.add(sphereMesh);
 
-//initialize the pane
-const pane = new Pane();
+//add random geo to scene
+const plusButton = document.querySelector("#plus-button");
+const inputText = document.querySelector("#input-text");
 
-pane.addInput(cubeMaterial, "metalness", {
-  min: 0,
-  max: 1,
-  step: 0.01,
-});
-pane.addInput(cubeMaterial, "roughness", {
-  min: 0,
-  max: 1,
-  step: 0.01,
-});
-pane.addInput(cubeMaterial, "opacity", {
-  min: 0,
-  max: 1,
-  step: 0.01,
-});
-pane.addInput(cubeMaterial, "clearcoat", {
-  min: 0,
-  max: 1,
-  step: 0.01,
-});
-pane.addInput(cubeMaterial, "transparent", {
-  options: { on: "true", off: "false" },
-});
+const addRandomMesh = () => {
+  const randomMeshArr = [
+    new THREE.Mesh(
+      new THREE.TorusKnotGeometry(0.4, 0.15, 100, 16),
+      cubeMaterial
+    ),
+    new THREE.Mesh(new THREE.ConeGeometry(0.6, 1.2, 25), cubeMaterial),
+    new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.2, 20), cubeMaterial),
+  ];
+  const i = Math.floor(Math.random() * 3);
+  randomMesh = randomMeshArr[i];
+  scene.add(randomMesh);
 
-// const loader = new GLTFLoader();
-// loader.load("pillow.gltf", function (gltf) {
-//   const model = gltf.scene;
-//   scene.add(model);
-//   model.position.set(0, 0, 0);
-// });
+  rotatingMeshes.push(randomMesh);
+  console.log(rotatingMeshes);
+};
 
-// const fog = new THREE.Fog(0xffffff, 1, 20);
-// scene.fog = fog;
+const handleClick = () => {
+  if (inputText.value.trim() === "") {
+    inputText.classList.add("error");
+    setTimeout(() => {
+      inputText.classList.remove("error");
+    }, 1000);
+    return;
+  }
+
+  inputText.value = "";
+  addRandomMesh();
+};
+
+plusButton.addEventListener("click", handleClick);
 
 scene.background = new THREE.Color(0xeee1d4);
-
-//cubeMaterial.fog = false; set mesh to be affected by fog
 
 const light = new THREE.AmbientLight(0xffffff, 2);
 scene.add(light);
@@ -85,7 +82,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
-camera.position.z = 20;
+camera.position.z = 15;
 
 //get html element
 const canvas = document.querySelector("canvas.threejs");
@@ -99,31 +96,27 @@ renderer.setPixelRatio(maxPixelRatico);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true; //call .update () in render loop
 
-//add helper
-const axesHelper = new THREE.AxesHelper(1);
-scene.add(axesHelper);
-
 //initiate render size
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //window event, spare the resizing being called every frame
 window.addEventListener("resize", () => {
-  console.log(window.innerWidth / window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix(); //needed for the para update for camera
   renderer.setSize(window.innerWidth, window.innerHeight); //update
 });
 
-const clock = new THREE.Clock();
-
 const renderloop = () => {
-  const elapsedTime = clock.getElapsedTime();
+  rotatingMeshes.forEach((mesh, index) => {
+    mesh.rotation.y += 0.01;
 
-  coneMesh.position.x = Math.sin(elapsedTime) * 4;
-  coneMesh.position.z = Math.cos(elapsedTime) * 4;
-  torusKnotMesh.rotation.y += 0.05;
-  torusKnotMesh.position.x = Math.sin(torusKnotMesh.rotation.y) * 2;
-  torusKnotMesh.position.z = Math.cos(torusKnotMesh.rotation.y) * 2;
+    mesh.position.x = Math.sin(mesh.rotation.y) * (2 + index);
+    mesh.position.z = Math.cos(mesh.rotation.y) * (2 + index);
+  });
+
+  if (rotatingMeshes.length >= 2) {
+    faceMaterial.color.set(0x456456);
+  }
 
   controls.update(); // for controls.enableDamping
   renderer.render(scene, camera);
